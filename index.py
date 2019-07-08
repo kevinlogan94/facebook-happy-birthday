71388bdc67fbf1e3fd2eb604c97710eeabb0b7e4
Kevin Logan <kevlogan7@gmail.com>
2019-07-08 12:56:19 -0400

    alphabetize imports

--- index.py
+++ index.py
@@ -6,12 +6,12 @@ from actions import check_for_brave_new_tab_dark_mode
 from actions import close_tab
 from actions import notification
 from actions import open_brave
-from actions import open_new_tab
-from actions import type_in_url
 from actions import clear_mouse
+from actions import open_new_tab
+from actions import send_happy_birthday_message
+from actions import type_in_url
 from actions import wait_for_browser_to_process_happy_birthdays
 from actions import wish_everyone_happy_birthday
-from actions import send_happy_birthday_message
 
 try:
     notification(
